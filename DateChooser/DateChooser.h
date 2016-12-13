bae0da999bc8472d19cde12e27b22dadab02a7c1
Ben Norris <code@bsn.design>
2016-12-13 13:10:03 -0700

    Add empty framework

---
+++ DateChooser/DateChooser.h
@@ -0,0 +1,19 @@
+//
+//  DateChooser.h
+//  DateChooser
+//
+//  Created by Ben Norris on 12/13/16.
+//  Copyright © 2016 BSN Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateChooser.
+FOUNDATION_EXPORT double DateChooserVersionNumber;
+
+//! Project version string for DateChooser.
+FOUNDATION_EXPORT const unsigned char DateChooserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateChooser/PublicHeader.h>
+
+
